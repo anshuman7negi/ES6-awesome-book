d60e1ea5fb8af37daefda80d31a7de8f82fe823b
anshuman7negi <anshumannegi777@gmail.com>
2023-07-10 20:57:05 +0530

    Solved linter errors

--- modules/clock.js
+++ modules/clock.js
@@ -2,11 +2,11 @@ import { time } from './variable.js';
 // eslint-disable-next-line import/no-unresolved
 import { DateTime } from './luxon.js';
 
- const displayClock = () => {
+const displayClock = () => {
   setInterval(() => {
     const timeNow = DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss');
     time.innerHTML = timeNow;
   }, 1);
-}
+};
 
 export default displayClock;
