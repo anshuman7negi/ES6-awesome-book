17477d95a2a36a019e1ae75148dc12300f45f350
anshuman7negi <anshumannegi777@gmail.com>
2023-07-10 20:12:38 +0530

    added script to index.html file

--- modules/clock.js
+++ modules/clock.js
@@ -2,9 +2,11 @@ import { time } from './variable.js';
 // eslint-disable-next-line import/no-unresolved
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
-export default function displayClock() {
+ const displayClock = () => {
   setInterval(() => {
     const timeNow = DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss');
     time.innerHTML = timeNow;
   }, 1);
 }
+
+export default displayClock;
